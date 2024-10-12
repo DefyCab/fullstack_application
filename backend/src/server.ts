098f65bce0134b6e8058228a97b320e712364835
defycab <love.bellinder@gmail.com>
2024-10-12 14:49:34 +0200

    add convertToAnagram function

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,17 @@
 import http from "http";
 
+function convertToAnagram(name: string) {
+  const anagram = name.split("").sort(() => 0.5 - Math.random());
+  return anagram;
+}
+
 const server = http.createServer((req, res) => {
   if (req.url) {
     const formUrl = req.url;
     // possible split using regex for letters
+    // todo: regex to ensure name is just letters
     const name: string = formUrl.split("=")[1];
-    const nameJson :any = JSON.stringify({ "name": name})
+    const nameJson: any = JSON.stringify({ name: name });
     res.end(nameJson);
   }
 });
