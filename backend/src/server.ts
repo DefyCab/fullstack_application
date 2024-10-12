2e1a2b5c2a752ccb0fe588a9f0d96979db9e8bdf
defycab <love.bellinder@gmail.com>
2024-10-12 15:46:38 +0200

    formatting index.html and fixing minor naming issues

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import http from "http";
 
+// todo: read in file with words and make anagram that is a real word if exist in wordlist
 function convertToAnagram(name: string) {
   const anagram = name.split("").sort(() => 0.5 - Math.random());
   return anagram;
