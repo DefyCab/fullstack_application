ad86a0d0e4f8fbebc450bdef61985282ab9c3cdf
defycab <love.bellinder@gmail.com>
2024-10-13 22:41:25 +0200

    minor refactoring. add back describe block

--- backend/src/server.test.ts
+++ backend/src/server.test.ts
@@ -2,21 +2,22 @@ import { describe, it } from "node:test";
 import { deepEqual } from "node:assert";
 import { convertToAnagram } from "./functions";
 
-it("convertToAnagram function returns a string", () => {
-  const anagram = convertToAnagram("Love");
-  const result = (anagram: string) => {
-    anagram.split("").forEach((letter) => {
-      !anagram.includes(letter);
-      return;
-    });
-    return anagram;
-  };
+describe("Testing functions", () => {
+  it("convertToAnagram returns a string with the same letters", () => {
+    const anagram = convertToAnagram("Love");
+    const result = (anagram: string) => {
+      anagram.split("").forEach((letter) => {
+        !anagram.includes(letter);
+        return;
+      });
+      return anagram;
+    };
 
-  const match = result(anagram);
+    const match = result(anagram);
 
-  deepEqual(anagram, match);
+    deepEqual(anagram, match);
+  });
 });
-it("convertToAnagram returns a scrambled name", () => {});
 
 describe("Server actions", () => {
   it("the server should responds with statuscode 200", () => {});
