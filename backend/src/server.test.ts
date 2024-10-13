ae2b88baccecd55ac2a0c56d940b0211bba5af58
defycab <love.bellinder@gmail.com>
2024-10-13 10:49:55 +0200

    change to it keyword in tests

--- backend/src/server.test.ts
+++ backend/src/server.test.ts
@@ -1,14 +1,14 @@
-import test, { describe } from "node:test";
+import { describe, it } from "node:test";
 import { deepEqual } from "node:assert";
 
 describe("Functions", () => {
-  test("convertToAnagram function returns a string", () => {
+  it("convertToAnagram function returns a string", () => {
     deepEqual(1, 1);
   });
-  test("convertToAnagram returns a scrambled name", () => {});
+  it("convertToAnagram returns a scrambled name", () => {});
 });
 
 describe("Server actions", () => {
-  test("the server responds with statuscode 200", () => {});
-  test("the server responds with a JSON object", () => {});
+  it("the server should responds with statuscode 200", () => {});
+  it("the server responds with a JSON object", () => {});
 });
