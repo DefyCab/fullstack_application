2d439e371032c31ec5da79084af8012001e99b71
defycab <love.bellinder@gmail.com>
2024-10-14 21:18:20 +0200

    import mongoose and cors,connecting database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,22 @@
-import express from "express"
-const port = 8080
-const app = express()
+import express from "express";
+import cors from "cors"
+const port = 8080;
+const app = express();
+import mongoose from "mongoose";
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Anagram paradise!"')
-})
+mongoose.connect("mongodb://localhost:8080/anagrams");
+const db = mongoose.connection;
+
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to local database."));
+
+app.use(express.json());
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.send('Welcome to Anagram paradise!"');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server is running. ${port}`);
+});
